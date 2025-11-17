3131761b2123db92c1bd16bd825a8811f9ef4b59
srishti0203-hue <srishti0203@gmail.com>
2025-11-17 11:24:45 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,6 @@
+const [notifications, setNotifications] = useState([]);
+const [unreadCount, setUnreadCount] = useState(0);
+const [showNotifPanel, setShowNotifPanel] = useState(false);
 import React, { useEffect, useState, useRef } from "react";
 import "./index.css";
 
